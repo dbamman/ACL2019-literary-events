4ccd5efc482141418687a5ac419fedb65df90201
David Bamman <david.bamman@gmail.com>
2019-05-31 14:11:55 -0700

    init

---
+++ analysis/results/violin.r
@@ -0,0 +1,4 @@
+library(ggplot2)
+data=read.table("prestige.txt", head=TRUE, sep="\t", quote="")
+data$prestige=as.factor(data$prestige)
+ggplot(data, aes(x=prestige, y=100*val)) + geom_violin() + xlab("") + ylab("Event ratio") + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.3) + stat_summary(fun.y=median, geom="point", size=2, color="red") + theme(axis.title.y=element_text(margin = margin(t = 0, r = 12, b = 0, l = 0))
