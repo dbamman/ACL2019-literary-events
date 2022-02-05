e44f21845879144322584ac5b004510654bd7008
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-05 13:56:46 +0800

    Delete scripts directory

--- scripts/eval_verb_model.sh
+++
@@ -1,2 +0,0 @@
-python verb_only_baseline.py ../results/verb_only_preds.txt
-python event_eval.py ../results/verb_only_preds.txt > ../results/verb_only_preds.bootstrap.txt
--- scripts/get_gutenberg_embeddings.sh
+++
@@ -1,5 +0,0 @@
-# get skipgram embeddings trained on 15K texts from Project Gutenberg
-
-wget http://people.ischool.berkeley.edu/~dbamman/data/guten.vectors.txt.gz
-gunzip guten.vectors.txt.gz
-mv guten.vectors.txt ../data/
